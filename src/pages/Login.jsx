8f2c9ae342b9deafebfd093f38e37681e8f0cc14
star77ksj <star77ksj@users.noreply.github.com>
2026-06-13 13:30:56 +0900

    feat: implement login and signup page components

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Login() {
   return (
-    <div>
+    <div style={{ padding: '20px' }}>
       <h1>교사 로그인</h1>
       {/* TODO: 이메일, 비밀번호 폼 구현 */}
+      
+      <div style={{ marginTop: '20px' }}>
+        <p>학생이신가요?</p>
+        <Link to="/signup">회원가입 페이지로 이동하기</Link>
+      </div>
     </div>
   );
 }
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Signup() {
   return (
-    <div>
+    <div style={{ padding: '20px' }}>
       <h1>학생 회원가입</h1>
       {/* TODO: 이메일, 비밀번호 폼 구현 */}
+      
+      <div style={{ marginTop: '20px' }}>
+        <p>이미 계정이 있으신가요?</p>
+        <Link to="/login">로그인 페이지로 이동하기</Link>
+      </div>
     </div>
   );
 }
